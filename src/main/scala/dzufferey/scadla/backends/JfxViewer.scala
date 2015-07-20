63cf62735b32952ddab6530905059ec259afaa90
Damien Zufferey <zufferey@csail.mit.edu>
2015-07-19 22:14:27 -0400

    working on displaying assembly

--- src/main/scala/dzufferey/scadla/backends/JfxViewer.scala
+++ src/main/scala/dzufferey/scadla/backends/JfxViewer.scala
@@ -12,7 +12,7 @@ import scalafx.scene.image.Image
 import scalafx.scene.input.{MouseEvent,ScrollEvent}
 import scalafx.scene.paint.{Color, PhongMaterial}
 import scalafx.scene.shape.{MeshView, TriangleMesh}
-import scalafx.scene.transform.{Rotate,Scale}
+import scalafx.scene.transform.{Rotate,Scale,Translate}
 import scalafx.scene.{AmbientLight, Group, Node, PerspectiveCamera, PointLight, Scene, SceneAntialiasing}
 
 object JfxViewer extends Viewer {
@@ -45,13 +45,14 @@ object JfxViewer extends Viewer {
 
 //for testing
 //def main(args: Array[String]) {
-//  val obj = OpenSCAD(Union(Translate(-2,-2,-2,Sphere(5.0)), Cube(1.0, 1.0, 1.0)))
+//  val obj = OpenSCAD(Union(dzufferey.scadla.Translate(-2,-2,-2,Sphere(5.0)), Cube(1.0, 1.0, 1.0)))
 //  apply(obj)
 //}
 
 }
 
 //TODO add parameters from the JfxViewerObj
+//TODO arrows to display the axis
 object JfxViewerApp extends JFXApp {
 
   protected def readPolyFromStdIn = {
@@ -126,19 +127,19 @@ object JfxViewerApp extends JFXApp {
 
   /** Add mouse interaction to a scene, rotating given node. */
   def addMouseEvents(scene: Scene, node: Node) {
-    val angleZ = DoubleProperty(0)
     val zRotate = new Rotate {
-      angle <== angleZ
+      angle = 0
       axis = Rotate.ZAxis
     }
+    val angleZ = zRotate.angle
     var anchorX: Double = 0
     var anchorAngleZ: Double = 0
     
-    val angleX = DoubleProperty(90)
     val xRotate = new Rotate {
-      angle <== angleX
+      angle = 90
       axis = Rotate.XAxis
     }
+    val angleX = xRotate.angle
     var anchorY: Double = 0
     var anchorAngleX: Double = 0
 
@@ -185,14 +186,76 @@ case class JfxViewerObjPoly(p: Polyhedron) extends JfxViewerObj {
 }
 //TODO for displaying Assembly
 case class JfxViewerObjAssembly(a: Assembly) extends JfxViewerObj {
+  
+  protected def setFrame( frame: Frame,
+                          translate: (DoubleProperty, DoubleProperty, DoubleProperty),
+                          rotate: (DoubleProperty, DoubleProperty, DoubleProperty)) = {
+    val rpy = frame.orientation.toRollPitchYaw
+    translate._1() = frame.translation.x
+    translate._2() = frame.translation.y
+    translate._3() = frame.translation.z
+    rotate._1() = rpy.x
+    rotate._2() = rpy.y
+    rotate._3() = rpy.z
+  }
+
+  /** triple (T,R,M) where T controls the translation, R the rotation, and M is the mesh.
+   *  turn the Seq[(Frame,Polyhedron)] into a series of object and tranforms (and keep refs for the update).
+   */
+  lazy val meshes = a.at(0).map{ case (frame, poly) =>
+    val m0 = JfxViewerObj.objToMeshView(poly)
+    val angleX = DoubleProperty(0)
+    val angleY = DoubleProperty(0)
+    val angleZ = DoubleProperty(0)
+    val translate = new Translate(0,0,0)
+    val translateProps = (translate.x, translate.y, translate.z)
+    val rotateX = new Rotate { axis = Rotate.XAxis }
+    val rotateY = new Rotate { axis = Rotate.YAxis }
+    val rotateZ = new Rotate { axis = Rotate.ZAxis }
+    val rotateProps = (rotateX.angle, rotateY.angle, rotateZ.angle)
+    m0.transforms = Seq(rotateZ, rotateY, rotateX, translate) //TODO not sure of the order
+    setFrame(frame, translateProps, rotateProps)
+    (translateProps, rotateProps, m0)
+  }
+
   def parameters = List(("time", 0, 10), ("expansion",0,1))
   def setParameters(params: List[Double]) {
-    //TODO when updating, only the values of the tranforms needs to be updated
-    ???
+    params match {
+      case List(t, e) =>
+        val frames = a.expandAt(e, t)
+        //when updating, only the values of the tranforms needs to be updated
+        meshes.zip(frames).foreach{ case ((t,r,_),(f,_)) => setFrame(f, t, r) }
+      case other => sys.error("expected two parameters: " + other)
+    }
   }
-  //TODO turn the Seq[(Frame,Polyhedron)] into a series of object and tranforms (and keep a ref for the update)
-  def mesh = ???
-  def boundingBox = ??? 
+  def mesh = {
+    val ms = meshes.map(_._3)
+    new Group(ms:_*)
+  }
+  def boundingBox = {
+    import math.{min, max}
+    val bbs = a.at(0).map{ case (frame, poly) =>
+      val ((x1,x2), (y1,y2), (z1,z2)) = JfxViewerObj.getBoundingBox(poly)
+      val v0 = frame.directTo(Point(x1,y1,z1))
+      val vs = List(Point(x1,y1,z2),Point(x1,y2,z1),Point(x1,y2,z2),
+                    Point(x2,y1,z1),Point(x2,y1,z2),Point(x2,y2,z1),Point(x2,y2,z2))
+      vs.foldLeft(((v0.x,v0.x),(v0.y,v0.y),(v0.y,v0.z)))( (acc, p) => {
+        val p0 = frame.directTo(p)
+        ((min(acc._1._1, p0.x), max(acc._1._2, p0.x)),
+         (min(acc._2._1, p0.y), max(acc._2._2, p0.y)),
+         (min(acc._3._1, p0.z), max(acc._3._2, p0.z)))
+      })
+    }
+    if (bbs.isEmpty) {
+      ( (0.0, 1.0),
+        (0.0, 1.0),
+        (0.0, 1.0) )
+    } else bbs.reduce( (b1, b2) => {
+      ((min(b1._1._1, b2._1._1), max(b1._1._2, b2._1._2)),
+       (min(b1._2._1, b2._2._1), max(b1._2._2, b2._2._2)),
+       (min(b1._3._1, b2._3._1), max(b1._3._2, b2._3._2)))
+    })
+  }
 }
 
 object JfxViewerObj {
