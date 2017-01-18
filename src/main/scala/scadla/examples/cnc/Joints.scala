a223506bbf2e6cc031f53a20a4a6e68fb8b40d48
Damien Zufferey <zufferey@csail.mit.edu>
2017-01-18 22:15:59 +0100

    example

--- src/main/scala/scadla/examples/cnc/Joints.scala
+++ src/main/scala/scadla/examples/cnc/Joints.scala
@@ -27,7 +27,6 @@ class Joint2DOF(bottomNut: Double = Thread.ISO.M8) {
   //println("size:" + 2*outerRadius) → 21.28823512814129
 
   def cross(length: Double, height: Double, screwRadius: Double): Solid = {
-    //XXX hex instead of cubes ???
     val h = Hexagon(height/2, length).moveZ(-length/2)
     val c1 = h.rotateX(Pi/2)
     val c2 = h.rotateZ(Pi/6).rotateY(Pi/2)
@@ -38,7 +37,7 @@ class Joint2DOF(bottomNut: Double = Thread.ISO.M8) {
   }
 
   def cross: Solid = {
-    cross(2*(minRadius - washerThickness), 2*sideWall, M3-0.5)
+    cross(2*(minRadius - washerThickness - tightTolerance), 2*sideWall+1, M3-0.5)
   }
 
   protected def carving(top: Double, bottom: Double) = {
@@ -48,20 +47,22 @@ class Joint2DOF(bottomNut: Double = Thread.ISO.M8) {
   }
 
   protected def addScrewThingy(base: Solid, height: Double) = {
-    val c0 = Cylinder(2*M3-0.01,2*outerRadius).moveZ(-outerRadius).rotateY(Pi/2).moveZ(height)
-    val c1 = Cylinder(2*M3,2*outerRadius-2).moveZ(-outerRadius+1).rotateY(Pi/2).moveZ(height)
-    val c2 = Cylinder(2*M3+0.01,2*minRadius).moveZ(-minRadius).rotateY(Pi/2).moveZ(height)
-    val c3 = Cylinder(M3, 2*outerRadius+2).moveZ(-outerRadius-1).rotateY(Pi/2).moveZ(height)
-    val sh = Cylinder(2*M3+0.01, sideWall).rotateY(Pi/2).moveZ(height)
-    base - c0 + c1 - c2 - c3 - sh.moveX(outerRadius - 1.5) - sh.moveX(-outerRadius-sideWall + 1.5)
+    val r = 2*M3 + 2
+    val l = 2 * outerRadius
+    val l2 = outerRadius
+    val c0 = Cylinder(r-0.01,l+10).moveZ(-l2-5).rotateY(Pi/2).moveZ(height)
+    val c1 = Cylinder(r,l-2).moveZ(-l2+1).rotateY(Pi/2).moveZ(height)
+    val c2 = Cylinder(r+0.01,2*minRadius).moveZ(-minRadius).rotateY(Pi/2).moveZ(height)
+    val c3 = Cylinder(M3, l+10).moveZ(-l2-5).rotateY(Pi/2).moveZ(height)
+    val sh = Cylinder(r+looseTolerance, sideWall).rotateY(Pi/2).moveZ(height)
+    base - c0 + c1 - c2 - c3 - sh.moveX(outerRadius - 1) - sh.moveX(-outerRadius-sideWall + 1)
   }
 
   def bottom(height: Double) = {
     val nutTop = botWall + bottomNut * 1.5
     val base = Difference(
       Cylinder(outerRadius, height),
-      Hexagon(minRadius, height).moveZ(nutTop).rotateZ(Pi/6),
-      nut(bottomNut).moveZ(botWall).rotateZ(Pi/6),
+      Hexagon(minRadius, height).moveZ(botWall).rotateZ(Pi/6),
       Cylinder(bottomNut + tolerance, height),
       carving(height, nutTop).move(0,  outerRadius, nutTop - 0.5),
       carving(height, nutTop).move(0, -outerRadius, nutTop - 0.5)
