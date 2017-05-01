af3b63deae1966f24c5bfa83f8204af6aee7c5af
Damien Zufferey <zufferey@mpi-sws.org>
2017-05-01 16:56:46 +0200

    tidying code

--- src/main/scala/scadla/utils/PieSlice.scala
+++ src/main/scala/scadla/utils/PieSlice.scala
@@ -2,43 +2,47 @@ package scadla.utils
   
 import scadla._
 import scadla.InlineOps._
-import squants.space.Length
-import squants.space.Angle
+import scadla.utils.Trig._
+import squants.space.{Length, Angle}
 import scala.language.postfixOps
 import squants.space.LengthConversions._
-import squants.space.Radians
 
 object PieSlice {
 
   def apply(outerRadius: Length, innerRadius: Length, angle: Angle, height: Length) = {
     val o1 = outerRadius + (1 mm)
-    val t = Tube(outerRadius, innerRadius, height)
-    val blocking_half = Translate(-o1, -o1, -0.5 mm, Cube(2* o1, o1, height + (1 mm)))
-    val blocking_quarter = Translate(0 mm, 0 mm, -0.5 mm, Cube(o1, o1, height + (1 mm)))
+    val h1 = height + (1 mm)
+    val blocking_half = Cube(2* o1, o1, h1).move(-o1, -o1, -0.5 mm)
+    val blocking_quarter = Cube(o1, o1, h1).move(0 mm, 0 mm, -0.5 mm)
     if (angle.value <= 0) {
       Empty
     } else {
       val block =
-        if (angle <= Radians(math.Pi/2)) {
+        if (angle <= Pi/2) {
           Union(
             blocking_half,
-            Translate(-o1, -0.5 mm, 0 mm, blocking_quarter),
-            Rotate(0°, 0°, angle, blocking_quarter))
-        } else if (angle <= Radians(math.Pi)) {
+            blocking_quarter.move(-o1, -0.5 mm, 0 mm),
+            blocking_quarter.rotateZ(angle)
+          )
+        } else if (angle <= Pi) {
           Union(
             blocking_half,
-            Rotate(0°, 0°, angle, blocking_quarter))
-        } else if (angle <= Radians(3*math.Pi/2)) {
+            blocking_quarter.rotateZ(angle)
+          )
+        } else if (angle <= 3*Pi/2) {
           Union(
-            Translate(0 mm, -o1, 0 mm, blocking_quarter),
-            Rotate(0°, 0°, angle, blocking_quarter))
-        } else if (angle <= Radians(2*math.Pi)) {
+            blocking_quarter.moveY(-o1),
+            blocking_quarter.rotateZ(angle)
+          )
+        } else if (angle <= 2*Pi) {
           Intersection(
-            Translate(0 mm, -o1, 0 mm, blocking_quarter),
-            Rotate(0°, 0°, angle, blocking_quarter))
+            blocking_quarter.moveY(-o1),
+            blocking_quarter.rotateZ(angle)
+          )
         } else {
           Empty
         }
+      val t = Tube(outerRadius, innerRadius, height)
       Difference(t, block)
     }
   }
