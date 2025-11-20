775a79acc341d02c883ee3f8c640493ca80e995d
Damien Zufferey <rilaak@gmail.com>
2025-11-20 19:52:48 +0100

    add type annotation of implicit method

--- src/main/scala/scadla/EverythingIsIn.scala
+++ src/main/scala/scadla/EverythingIsIn.scala
@@ -1,10 +1,11 @@
 package scadla
 
 import scala.language.implicitConversions
+import squants.space.{Angle,Length,Millimeters,Inches,Radians,Degrees}
 
 object EverythingIsIn {
-  implicit def millimeters[A](n: A)(implicit num: Numeric[A]) = squants.space.Millimeters(n)
-  implicit def inches[A](n: A)(implicit num: Numeric[A]) = squants.space.Inches(n)
-  implicit def radians[A](n: A)(implicit num: Numeric[A]) = squants.space.Radians(n)
-  implicit def degrees[A](n: A)(implicit num: Numeric[A]) = squants.space.Degrees(n)
+  implicit def millimeters[A](n: A)(implicit num: Numeric[A]): Length = Millimeters(n)
+  implicit def inches[A](n: A)(implicit num: Numeric[A]): Length = Inches(n)
+  implicit def radians[A](n: A)(implicit num: Numeric[A]): Angle = Radians(n)
+  implicit def degrees[A](n: A)(implicit num: Numeric[A]): Angle = Degrees(n)
 }
