01d98f25c29331a2a27207c98e0d22061a5788b3
Damien Zufferey <rilaak@gmail.com>
2026-01-17 22:55:05 +0100

    syntax clean-up in openscad

--- src/test/scala/scadla/backends/OpenSCADTest.scala
+++ src/test/scala/scadla/backends/OpenSCADTest.scala
@@ -4,7 +4,6 @@ import scadla.*
 import org.scalatest.funsuite.AnyFunSuite
 import scadla.EverythingIsIn.{millimeters, radians}
 
-
 class OpenSCADTest extends AnyFunSuite {
 
   test("rendering a cube") {
@@ -16,41 +15,41 @@ class OpenSCADTest extends AnyFunSuite {
     }
   }
 
-//test("rendering 1a") {
-//  val obj = OpenSCADnightly(Sphere(1.0))
-//  x3d.X3DomViewer(obj)
-//}
+  // test("rendering 1a") {
+  //   val obj = OpenSCAD(Sphere(1.0))
+  //   x3d.X3DomViewer(obj)
+  // }
 
-//test("rendering 1b") {
-//  OpenSCAD.view(Sphere(1.0))
-//}
+  // test("rendering 1b") {
+  //   OpenSCAD.view(Sphere(1.0))
+  // }
 
-//test("rendering 2a") {
-//  val obj = Intersection(
-//              Union(
-//                Cube(1,1,1),
-//                Translate( -0.5, -0.5, 0, Cube(1,1,1))
-//              ),
-//              Sphere(1.5)
-//            )
-//  val mesh = OpenSCADnightly(obj)
-//  x3d.X3DomViewer(mesh)
-//}
+  // test("rendering 2a") {
+  //   val obj = Intersection(
+  //     Union(
+  //       Cube(1, 1, 1),
+  //       Translate(-0.5, -0.5, 0, Cube(1, 1, 1))
+  //     ),
+  //     Sphere(1.5)
+  //   )
+  //   val mesh = OpenSCAD(obj)
+  //   x3d.X3DomViewer(mesh)
+  // }
 
-//test("rendering 2b") {
-//  val c = Cube(1,1,1)
-//  val s = Sphere(1.5)
-//  val u = Union(c, Translate( -0.5, -0.5, 0, c))
-//  val obj = Intersection(u, s)
-//  OpenSCAD.view(obj)
-//}
+  // test("rendering 2b") {
+  //   val c   = Cube(1, 1, 1)
+  //   val s   = Sphere(1.5)
+  //   val u   = Union(c, Translate(-0.5, -0.5, 0, c))
+  //   val obj = Intersection(u, s)
+  //   OpenSCAD.view(obj)
+  // }
 
-//test("rendering 2c") {
-//  import InlineOps.*
-//  val c = Cube(1,1,1)
-//  val s = Sphere(1.5)
-//  val obj = (c + c.move( -0.5, -0.5, 0)) * s
-//  OpenSCAD.view(obj)
-//}
+  // test("rendering 2c") {
+  //   import InlineOps.*
+  //   val c   = Cube(1, 1, 1)
+  //   val s   = Sphere(1.5)
+  //   val obj = (c + c.move(-0.5, -0.5, 0)) * s
+  //   OpenSCAD.view(obj)
+  // }
 
 }
